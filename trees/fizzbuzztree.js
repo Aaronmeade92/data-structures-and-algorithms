0cd99e09e30ee07f37830bd40dc9f3f7083872ae
Aaron <aaronmeade9292@gmail.com>
2018-07-23 18:07:01 -0700

    added find and insert

--- trees/fizzbuzztree.js
+++ trees/fizzbuzztree.js
@@ -1,18 +1,66 @@
-class BinarySearchTree{
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
 
-    constructor(){
+class Tree {
+
+    constructor() {
         this.root = null;
     }
 
-    insert(data){
-        
+    insert(data) {
+
         var newNode = new Node(data);
 
-        if(this.root === null){
+        if (this.root === null) {
             this.rooot = newNode;
-        }else{
-         this.insertNode(this.root, newNode);
+        } else {
+            this.insertNode(this.root, newNode);
+        } return newNode;
+
+    };
+
+    remove(data) {
+        this.root = this.removeNode(this.root, data);
+    };
+
+    find(value) {
+        return contain(this.root, value);
+    }
+};
+
+
+function removeNode(node, data) {
+
+}
+
+function contains(node, data) {
+    if (node.value === data) {
+        return true;
+    }
+    if (n < node.value) {
+        if (!node.right) {
+            return false
+        } else {
+            return contain(node.right, data);
         }
-        
+    } else {
+        if (!node.left) {
+            return false
+        } else {
+            return contain(node.left, data);
+        }
     }
 }
+// Tree.prototype.Traversal = function (filter) {
+
+//     let queue = [];
+//     let results = [];
+//     const nextNode;
+
+
+// };
